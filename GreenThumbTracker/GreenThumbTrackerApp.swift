0c883c8da671604d24600620d97c2333553e79c4
Toby Buckmaster <tdude37@me.com>
2025-03-20 13:46:12 -0500

    Initial Commit

---
+++ GreenThumbTracker/GreenThumbTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenThumbTrackerApp.swift
+//  GreenThumbTracker
+//
+//  Created by Toby Buckmaster on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct GreenThumbTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GreenThumbTrackerTests/GreenThumbTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  GreenThumbTrackerTests.swift
+//  GreenThumbTrackerTests
+//
+//  Created by Toby Buckmaster on 3/20/25.
+//
+
+import Testing
+@testable import GreenThumbTracker
+
+struct GreenThumbTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
