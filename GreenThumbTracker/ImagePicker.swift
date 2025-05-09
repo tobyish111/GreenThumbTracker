59a7eb6c4f43dfdd7187fa573a39358d7b6187d9
Toby Buckmaster <tdude37@me.com>
2025-05-08 23:46:59 -0500

    Last few bits. Fixed bugs and cleaned up some views.

---
+++ GreenThumbTracker/ImagePicker.swift
@@ -0,0 +1,8 @@
+//
+//  ImagePicker.swift
+//  GreenThumbTracker
+//
+//  Created by Toby Buckmaster on 5/8/25.
+//
+
+import Foundation
---
+++ GreenThumbTracker/PlantImageManager.swift
@@ -0,0 +1,8 @@
+//
+//  PlantImageManager.swift
+//  GreenThumbTracker
+//
+//  Created by Toby Buckmaster on 5/8/25.
+//
+
+import Foundation
